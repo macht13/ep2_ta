85ff4dd9fbe35523f5c9c78f81671e5707592a6a
Sebastian <mail.mail.pi@gmail.com>
2018-05-06 14:56:28 +0100

    Added next/prev Added null constructor

--- src/Junction.java
+++ src/Junction.java
@@ -1,4 +1,7 @@
 public class Junction {
+    // doppelt verkettete Liste
+    private Junction next, prev;
+
     private String name;
     private double xPos, yPos;
     private boolean isAirport;
@@ -8,6 +11,9 @@ public class Junction {
         this.xPos = xPos;
         this.yPos = yPos;
         this.isAirport = isAirport;
+    }
+
+    public Junction() {
     }
 
     public boolean isAirport() {
@@ -40,5 +46,21 @@ public class Junction {
 
     public void setyPos(double yPos) {
         this.yPos = yPos;
+    }
+
+    public Junction getNext() {
+        return next;
+    }
+
+    public Junction getPrev() {
+        return prev;
+    }
+
+    public void setNext(Junction next) {
+        this.next = next;
+    }
+
+    public void setPrev(Junction prev) {
+        this.prev = prev;
     }
 }
